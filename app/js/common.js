48214a5b952dbbc5f9d27b278cb228604734f8c0
devalmanu <manuilova-1986@mail.ru>
2020-05-29 13:35:54 +0300

    initial commit

---
+++ app/js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.nav__link').click(() => {
+		$('.submenu').fadeToggle(500)
+	});
+	$('.icon-search').click(() => {
+		$('.search-form').toggle(500)
+	})
+});
+
+
